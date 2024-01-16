12736154ff686e79f0b78dabd54f1c5cf9755cb2
Jérémie Crinon <jeremie.crinon@gmail.com>
2024-01-16 14:11:53 +0100

    Adding a logout button to the profile edit page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,10 +7,17 @@
         {{ __('Here, you can edit your profile information and change your password.') }}
     </p>
 
+    <form method="POST" action="{{ route('logout') }}" class="Edit_profile--logout">
+        @csrf
+        <button type="submit" class="Edit_profile--logout--button" >{{ __('Logout') }}</button>
+    </form>
+
     <div class="Edit_profile--container">
         @include('profile.partials.update-profile-information-form')          
         @include('profile.partials.update-password-form')
     </div>
+
+    
 
             <!-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
