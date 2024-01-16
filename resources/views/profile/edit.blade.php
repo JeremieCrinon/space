41e5b9409f78b5141ae2faf3a42360af70c851e5
Jérémie Crinon <jeremie.crinon@gmail.com>
2024-01-16 13:51:09 +0100

    Stylising the profile edit page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,23 +1,16 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+    @include('layouts.navigation', ['pageName' => "profile"])
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
-            </div>
+    <h1 class="Dashboard--title">{{ __('Edit your profile') }}</h1>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
+    <p class="Dashboard--description">
+        {{ __('Here, you can edit your profile information and change your password.') }}
+    </p>
+
+    <div class="Edit_profile--container">
+        @include('profile.partials.update-profile-information-form')          
+        @include('profile.partials.update-password-form')
+    </div>
 
             <!-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
