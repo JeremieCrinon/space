2dc622d6ea641793925cbfa52056aa09b63dfb00
Jérémie Crinon <jeremie.crinon@gmail.com>
2024-01-17 14:36:12 +0100

    Modifying the hader so it redirects to the first planet using it's id

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -17,12 +17,11 @@
     <nav class="Website--header--menu" role="navigation">
         <ul>
             <li @if(isset($pageName) && $pageName == "home")class="Website--header--li--current"@endif><a href="{{ url('/') }}"><span>00</span> {{ __('HOME') }}</a></li>
-            <li @if(isset($pageName) && $pageName == "destination")class="Website--header--li--current"@endif><a href="{{ url('/destination/moon') }}"><span>01</span> DESTINATION</a></li>
+            <li @if(isset($pageName) && $pageName == "destination")class="Website--header--li--current"@endif><a href="{{ url('/destination/' . $firstItemId) }}"><span>01</span> DESTINATION</a></li>
             <li @if(isset($pageName) && $pageName == "crew")class="Website--header--li--current"@endif><a href="{{ url('/crew/commander') }}"><span>02</span> {{ __('CREW') }}</a></li>
             <li @if(isset($pageName) && $pageName == "tech")class="Website--header--li--current"@endif><a href="{{ url('/tech/Launcher') }}"><span>03</span> {{ __('TECHNOLOGY') }}</a></li>
             <a class="Website--header--language" href="{{ route('language.switch', 'en') }}">English</a>
             <a class="Website--header--language" href="{{ route('language.switch', 'fr') }}">Français</a>
-
         </ul>
     </nav>    
 
